7246762546e4d5e6827bcd1f7b9e253bd2fdfe88
Konstantin Draganov <konstantinvld@gmail.com>
2015-03-12 01:31:41 +0000

    - minor improvements TODO: change update to ajax

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,28 +9,41 @@ class MainController < ApplicationController
     @lastModifiedTime = File.mtime(INPUT_FILENAME)
     @tableLastUpdated = Disruption.maximum('updated_at')
 
-    if(@tableLastUpdated == nil || @lastModifiedTime > @tableLastUpdated)
-
+    if (@tableLastUpdated == nil || @lastModifiedTime > @tableLastUpdated)
       Disruption.delete_all
+      disruptions = []
       CSV.foreach(INPUT_FILENAME, :headers => true, :col_sep => ',') do |row|
-        Disruption.create(:route => row['Route'],
-                          :direction => row['Direction'],
-                          :fromStopName => row['FromStopName'],
-                          :fromStopCode => row['FromStopCode'],
-                          :toStopName => row['ToStopName'],
-                          :toStopCode => row['ToStopCode'],
-                          :delay => row['DisruptionObserved'],
-                          :routeTotalDelay => row['RouteTotal'],
-                          :trend => row['Trend'],
-                          :timeFirstDetected => row['TimeFirstDetected'])
+        disruptions << Disruption.new(:route => row['Route'],
+                                      :direction => row['Direction'],
+                                      :fromStopName => row['FromStopName'],
+                                      :fromStopCode => row['FromStopCode'],
+                                      :toStopName => row['ToStopName'],
+                                      :toStopCode => row['ToStopCode'],
+                                      :delay => row['DisruptionObserved'],
+                                      :routeTotalDelay => row['RouteTotal'],
+                                      :trend => row['Trend'],
+                                      :timeFirstDetected => row['TimeFirstDetected'])
+        # list.append({:route => row['Route'],
+        #              :direction => row['Direction'],
+        #              :fromStopName => row['FromStopName'],
+        #              :fromStopCode => row['FromStopCode'],
+        #              :toStopName => row['ToStopName'],
+        #              :toStopCode => row['ToStopCode'],
+        #              :delay => row['DisruptionObserved'],
+        #              :routeTotalDelay => row['RouteTotal'],
+        #              :trend => row['Trend'],
+        #              :timeFirstDetected => row['TimeFirstDetected']})
       end
+
+      Disruption.import disruptions
+      # Disruption.create(list)
     end
 
     #Order descending by disruption time observed
     @disruptions = Disruption.order(delay: :desc, routeTotalDelay: :desc, timeFirstDetected: :desc).first(20)
 
     @totalNumberOfDisruptions = Disruption.count
-    @refreshTimeout = 5000 #30seconds
+    @refreshTimeout = 10000 #30seconds
   end
 
   def about
